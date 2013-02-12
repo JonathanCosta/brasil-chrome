115e1bfd856dca29abcd465e9c195b352271edd7
Renato Gama <renatoargh@gmail.com>
2013-02-11 23:44:28 -0200

    Primeiro commit

---
+++ content_script.js
@@ -0,0 +1,10 @@
+var clickedEl = null;
+document.addEventListener("mousedown", function(event){
+    if(event.button == 2) { 
+        clickedEl = event.target;
+    }
+}, true);
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    clickedEl.value = request;
+});

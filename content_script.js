be130cab9d077b112f0fe6b0a59383d6feb34233
Renato Gama <renatoargh@gmail.com>
2015-05-01 12:10:28 -0300

    Disparando evento ‘change’ após definir o valor no elemento

--- content_script.js
+++ content_script.js
@@ -1,5 +1,6 @@
 var clickedEl = null;
-document.addEventListener("mousedown", function(event){
+
+document.addEventListener('mousedown', function(event) {
     if(event.button == 2) {
         clickedEl = event.target;
     }
@@ -7,4 +8,11 @@ document.addEventListener("mousedown", function(event){
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     clickedEl.value = request;
+
+    var changeEvent = new Event('change', {
+        'bubbles': true,
+        'cancelable': false
+    });
+
+    clickedEl.dispatchEvent(changeEvent);
 });
